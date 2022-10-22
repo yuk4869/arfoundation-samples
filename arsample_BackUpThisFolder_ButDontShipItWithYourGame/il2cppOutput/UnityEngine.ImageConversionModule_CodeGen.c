96aec44a7f5a5356c72f11c4c8e4e331b8350cb2
Hiroyuki Ishizuka <56533309+yuk4869@users.noreply.github.com>
2022-10-23 08:41:56 +0900

    camera print

---
+++ arsample_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/UnityEngine.ImageConversionModule_CodeGen.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Byte[] UnityEngine.ImageConversion::EncodeToJPG(UnityEngine.Texture2D,System.Int32)
+extern void ImageConversion_EncodeToJPG_mD0307B5DFF32A3FF39488E97B467F11AFE501F6F (void);
+// 0x00000002 System.Byte[] UnityEngine.ImageConversion::EncodeToJPG(UnityEngine.Texture2D)
+extern void ImageConversion_EncodeToJPG_mD3B358B8645CF85EBAD979554FFDE05A54A99804 (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	ImageConversion_EncodeToJPG_mD0307B5DFF32A3FF39488E97B467F11AFE501F6F,
+	ImageConversion_EncodeToJPG_mD3B358B8645CF85EBAD979554FFDE05A54A99804,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	8355,
+	9134,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_ImageConversionModule_CodeGenModule = 
+{
+	"UnityEngine.ImageConversionModule.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
